024faeef648d14d15631b452bb9d7e0af124edcb
Neil <mitsuru.jack@gmail.com>
2022-11-20 15:18:34 +0900

    name test fixed

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,9 +9,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     get contact_path
-    assert_select "title", full_title("Contact")
+    assert_select "title", full_title(I18n.t('views.static_pages.contact.title'))
     get signup_path
-    assert_select "title", full_title("Sign up")
+    assert_select "title", full_title(I18n.t('views.users.new.title'))
   end
   
   def setup
